06811cc3860a23603c238be0f4cf59d7d02e4e77
imaaa34 <oct.imd34@gmail.com>
2021-07-19 10:46:48 +0900

    [Add] prop practice

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -19,5 +20,63 @@ function App() {
     </div>
   );
 }
+
+const Hello = (props) => {
+  // return <div>こんにちは、{props.name}さん</div>;
+  return <div>{props.name}さんの誕生日は{props.birthday}です</div>;
+};
+
+ReactDOM.render(
+  <div>
+    <Hello name="坂本龍馬" birthday="1836/01/03" />
+    <Hello name="西郷隆盛" />
+    <Hello name="勝海舟" />
+    <Hello name={"織田信長"} />
+    <Hello name={["坂本龍馬", "西郷隆盛"]} />
+  </div>,
+  document.getElementById('root')
+);
+
+const Hello2 = (props) => {
+  return (
+    <div>こんにちは、{props.children}さん</div>
+  );
+};
+
+const Greeting = (props) => {
+  return (
+    <div>
+      <div>ご挨拶</div>
+      {props.children}
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <Greeting>
+    <Hello2>坂本龍馬</Hello2>
+    <Hello2>西郷隆盛</Hello2>
+  </Greeting>,
+  document.getElementById('root')
+);
+
+const Profile = (props) => {
+  return (
+    <ul>
+      <li>名前: {props.name}</li>
+      <li>誕生日: {props.birthDay}</li>
+    </ul>
+  )
+}
+
+const profile = {
+  name: '坂本龍馬',
+  birthDay: '1836/01/03'
+};
+
+ReactDOM.render(
+  <Profile {...profile}/>,
+  document.getElementById('root')
+);
 
 export default App;
---
+++ src/practice.js
@@ -0,0 +1,30 @@
+// import ReactDOM from 'react-dom';
+
+// function Practice() {
+//   return (
+//     <div className="practice">
+//       <h1 className="practice-title">Hello, practice!</h1>
+//     </div>
+//   );
+// }
+
+// const Profile = (props) => {
+//   return (
+//     <ul>
+//       <li>名前: {props.name}</li>
+//       <li>誕生日: {props.birthDay}</li>
+//     </ul>
+//   )
+// }
+
+// const profile = {
+//   name: '坂本龍馬',
+//   birthDay: '1836/01/03'
+// };
+
+// ReactDOM.render(
+//   <Profile {...profile}/>,
+//   document.getElementById('root')
+// );
+
+// export default Practice;
